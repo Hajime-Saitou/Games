f387f9cfacf2f84e6e90203724c24f23d46de42e
Hajime-Saitou <g.hajime.saitou@gmail.com>
2021-02-07 18:58:01 +0900

    Add new class 'HighScoreRankingManager'

--- Python/Module/ScoreManager.py
+++ Python/Module/ScoreManager.py
@@ -38,3 +38,43 @@ class ExtendManager(object):
             if self.every > 0:
                 self.nextExtendScore += self.every
         return True
+
+class HighScoreRankingManager(object):
+    def __init__(self, validKeys, maxRecordCount=10):
+        if not "Score" in validKeys:
+            raise KeyError("Error: Do not have the key 'Score'.")
+
+        self.validKeys = validKeys
+        self.highScoreRecords = []
+
+        if maxRecordCount < 1:
+            raise ValueError("Error: Max record count sets positive value.")
+        self.maxRecordCount = maxRecordCount
+
+    def append(self, newRecord):
+        if newRecord.keys() ^ self.validKeys:
+            raise KeyError("Error: The number of keys does not match.")
+        if not self.isRankIn(newRecord["Score"]):
+            return -1
+
+        self.highScoreRecords.append(newRecord)
+        self.sortDesendingByScore()
+        return self.getNewRecordPosition(newRecord)
+
+    def sortDesendingByScore(self):
+        self.highScoreRecords = sorted(self.highScoreRecords, key=lambda k: k["Score"], reverse=True)[:self.maxRecordCount]
+
+    def getNewRecordPosition(self, newRecord):
+        for position, record in enumerate(self.highScoreRecords):
+            if record == newRecord:
+                return position
+
+        return -1
+
+    def isExceedTopScore(self, score):
+        return score >= self.highScoreRecords[0]["Score"]
+
+    def isRankIn(self, score):
+        if len(self.highScoreRecords) < self.maxRecordCount:
+            return True
+        return score >= self.highScoreRecords[-1]["Score"]
